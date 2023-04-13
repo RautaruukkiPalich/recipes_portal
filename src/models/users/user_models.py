412b249d5b9d5201426dc53c5dca2580dd10a750
RautaruukkiPalich <rautaruukkipalich@gmail.com>
2023-04-13 19:40:54 +0500

    remove back_populates

--- src/models/users/user_models.py
+++ src/models/users/user_models.py
@@ -9,7 +9,7 @@ class Role(TableMixin, Base):
     id = Column(Integer, primary_key=True)
     role = Column(String(50))
 
-    userRole = relationship("UserRole", back_populates="role")
+    userRole = relationship("UserRole")  # , back_populates="role")
 
     __table_args__ = (
         PrimaryKeyConstraint('id', name='role_id'),
@@ -17,26 +17,26 @@ class Role(TableMixin, Base):
     )
 
 
-class User(TableMixin, Base):
-
-    id = Column(Integer, primary_key=True)
-    nickname = Column(String(50), default="")
-    first_name = Column(String(50), default="")
-    last_name = Column(String(50), default="")
-    email = Column(String(length=320), unique=True, index=True, nullable=False)
-    hashed_password = Column(String(length=1024), nullable=False)
-    is_active = Column(Boolean, default=True, nullable=False)
-    is_deleted = Column(Boolean, default=False, nullable=False)
-    is_superuser = Column(Boolean, default=False, nullable=False)
-    is_verified = Column(Boolean, default=False, nullable=False)
-
-    userRole = relationship("UserRole", back_populates="user")
-    recipe = relationship("Recipe", back_populates="user")
-
-    __table_args__ = (
-        PrimaryKeyConstraint('id', name='user_id'),
-        {'extend_existing': True},
-    )
+# class User(TableMixin, Base):
+#
+#     id = Column(Integer, primary_key=True)
+#     nickname = Column(String(50), default="")
+#     first_name = Column(String(50), default="")
+#     last_name = Column(String(50), default="")
+#     email = Column(String(length=320), unique=True, index=True, nullable=False)
+#     hashed_password = Column(String(length=1024), nullable=False)
+#     is_active = Column(Boolean, default=True, nullable=False)
+#     is_deleted = Column(Boolean, default=False, nullable=False)
+#     is_superuser = Column(Boolean, default=False, nullable=False)
+#     is_verified = Column(Boolean, default=False, nullable=False)
+#
+#     userRole = relationship("UserRole")  # , back_populates="user")
+#     recipe = relationship(Recipe)  # , back_populates="user")
+#
+#     __table_args__ = (
+#         PrimaryKeyConstraint('id', name='user_id'),
+#         {'extend_existing': True},
+#     )
 
 
 class UserRole(TableMixin, Base):
@@ -45,8 +45,8 @@ class UserRole(TableMixin, Base):
     user_id = Column(Integer, ForeignKey("user.id"))
     role_id = Column(Integer, ForeignKey("role.id"))
 
-    user = relationship("User", back_populates="userRole")
-    role = relationship("Role", back_populates="userRole")
+    user = relationship("User")  # , back_populates="userRole")
+    role = relationship("Role")  # , back_populates="userRole")
 
     __table_args__ = (
         PrimaryKeyConstraint('id', name='userRole_id'),
