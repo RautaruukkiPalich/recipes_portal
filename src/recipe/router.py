c573bb0e99b0af043bae6815b629ea012438eaa8
RautaruukkiPalich <rautaruukkipalich@gmail.com>
2023-04-13 19:42:13 +0500

    rename schemas elems and replace funcs to services

--- src/recipe/router.py
+++ src/recipe/router.py
@@ -1,65 +1,38 @@
 import datetime
-import logging
 
 from fastapi import APIRouter, Depends
 from typing import List
-from sqlalchemy import select, insert, delete, update
+from sqlalchemy import select, insert, update
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
-
 from src.db.pg.settings import get_async_session
-from src.recipe.models import (Tag as tag,
-                               Ingredient as ingredient,
-                               Measure as measure,
-                               Recipe as recipe,
-                               )
-from src.recipe.schemas import (TagCreate,
-                                Tag,
-                                MeasureCreate,
-                                Measure,
-                                RecipeCreate,
-                                Recipe,
-                                IngredientCreate,
-                                Ingredient,
-                                )
+from src.recipe.models import (Tag, Ingredient, Measure, Recipe)
+from src.recipe.schemas import (TagCreateSchema, TagSchema, MeasureCreateSchema,
+                                MeasureSchema, RecipeCreateSchema, RecipeSchema,
+                                IngredientCreateSchema, IngredientSchema, RecipeFullSchema)
+from src.recipe.services import (get_sequence_from_db, create_error,
+                                 create_integrity_error, get_recipes_list)
 
 router = APIRouter(
-    prefix="/recipe",
-    tags=["recipe"]
+    prefix="/recipes",
+    tags=["recipes"]
 )
-
-
-async def create_integrity_error(e):
-    return {
-        "error": "error",
-        "desc": "integrity_error",
-        "status": 500,
-        "msg": e.orig,
-    }
-
-
-async def create_error(e):
-    return {
-        "error": "error",
-        "status": 500,
-        "msg": e,
-    }
 
 
 @router.post("/tags/")
 async def add_tag(
-        tag_name: TagCreate,
+        tag_name: TagCreateSchema,
         session: AsyncSession = Depends(get_async_session)
 ):
-    stmt = insert(tag).values(**tag_name.dict())
+    stmt = insert(Tag).values(**tag_name.dict())
     await session.execute(stmt)
     await session.commit()
     return {"status": "success"}
 
 
-@router.get("/tags/", response_model=List[Tag])
+@router.get("/tags/", response_model=List[TagSchema])
 async def get_tags(session: AsyncSession = Depends(get_async_session)):
-    query = select(tag)
+    query = select(Tag)
     result = await session.scalars(query)
     return result.all()
 
@@ -68,8 +41,8 @@ async def get_tags(session: AsyncSession = Depends(get_async_session)):
 async def del_tag(
         tag_id: int,
         session: AsyncSession = Depends(get_async_session)):
-    stmt = update(tag)\
-        .where(tag.id == tag_id)\
+    stmt = update(Tag)\
+        .where(Tag.id == tag_id)\
         .values(deleted_on=datetime.datetime.now())
     try:
         await session.execute(stmt)
@@ -89,8 +62,8 @@ async def del_tag(
 async def patch_tag(
         tag_id: int,
         session: AsyncSession = Depends(get_async_session)):
-    stmt = update(tag)\
-        .where(tag.id == tag_id)\
+    stmt = update(Tag)\
+        .where(Tag.id == tag_id)\
         .values(deleted_on=None)
     try:
         await session.execute(stmt)
@@ -108,10 +81,10 @@ async def patch_tag(
 
 @router.post("/measures/")
 async def add_measure(
-        measure_name: MeasureCreate,
+        measure_name: MeasureCreateSchema,
         session: AsyncSession = Depends(get_async_session)
 ):
-    stmt = insert(measure).values(**measure_name.dict())
+    stmt = insert(Measure).values(**measure_name.dict())
     try:
         await session.execute(stmt)
         await session.commit()
@@ -126,19 +99,19 @@ async def add_measure(
     return result
 
 
-@router.get("/measures/", response_model=List[Measure])
+@router.get("/measures/", response_model=List[MeasureSchema])
 async def get_measures(session: AsyncSession = Depends(get_async_session)):
-    query = select(measure)
+    query = select(Measure)
     result = await session.scalars(query)
     return result.all()
 
 
 @router.post("/ingredients/")
 async def add_ingredient(
-        ingredient_name: IngredientCreate,
+        ingredient_name: IngredientCreateSchema,
         session: AsyncSession = Depends(get_async_session)
 ):
-    stmt = insert(ingredient).values(**ingredient_name.dict())
+    stmt = insert(Ingredient).values(**ingredient_name.dict())
     try:
         await session.execute(stmt)
         await session.commit()
@@ -153,19 +126,19 @@ async def add_ingredient(
     return result
 
 
-@router.get("/ingredients/", response_model=List[Ingredient])
+@router.get("/ingredients/", response_model=List[IngredientSchema])
 async def get_ingredients(session: AsyncSession = Depends(get_async_session)):
-    query = select(ingredient)
+    query = select(Ingredient)
     result = await session.scalars(query)
     return result.all()
 
 
 @router.post("/")
 async def add_recipe(
-        recipe_items: RecipeCreate,
+        recipe_items: RecipeCreateSchema,
         session: AsyncSession = Depends(get_async_session)
 ):
-    stmt = insert(recipe).values(**recipe_items.dict())
+    stmt = insert(Recipe).values(**recipe_items.dict())
     try:
         await session.execute(stmt)
         await session.commit()
@@ -178,8 +151,15 @@ async def add_recipe(
     return result
 
 
-@router.get("/", response_model=List[Recipe])
-async def get_recipes(session: AsyncSession = Depends(get_async_session)):
-    query = select(recipe)
-    result = await session.scalars(query)
-    return result.all()
+@router.get("/", response_model=List[RecipeFullSchema])
+async def get_full_recipes(session: AsyncSession = Depends(get_async_session)):
+    result = await get_recipes_list(session)
+    return result
+
+
+@router.get("/{recipe_id}", response_model=List[RecipeSchema] | None)
+async def get_recipe_by_id(recipe_id: int, session: AsyncSession = Depends(get_async_session)):
+    query = select(Recipe).where(Recipe.id == recipe_id)
+    result = await get_sequence_from_db(session, query)
+    return result if result else None
+
