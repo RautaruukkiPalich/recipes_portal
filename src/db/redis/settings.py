a1bd1a10cbd67a647d6a08c4b1624a6297032cb5
RautaruukkiPalich <rautaruukkipalich@gmail.com>
2023-04-18 13:51:52 +0500

    add redis

---
+++ src/db/redis/settings.py
@@ -0,0 +1,10 @@
+import aioredis
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.redis import RedisBackend
+from fastapi_cache.decorator import cache
+
+
+async def start_redis():
+    redis = aioredis.from_url("redis://localhost", encoding="utf8", decode_responses=True)
+    FastAPICache.init(RedisBackend(redis), prefix="fastapi-cache")
+
