041305379bc613f346f2cedbf7a6da52d2c7b63b
RautaruukkiPalich <rautaruukkipalich@gmail.com>
2023-04-20 13:48:11 +0500

    remove

--- src/services/routers.py
+++ src/services/routers.py
@@ -1,38 +1,37 @@
-import json
-
-from fastapi import APIRouter, Request, Response
-from test_.tables import users
-
-router = APIRouter()
-
-
-@router.get("/", tags=["index"])
-async def start(request: Request) -> Response:
-    content = json.dumps({"data": "Hello world"})
-    return Response(
-        content=content,
-        status_code=200,
-        headers=request.headers,
-    )
-
+# import json
 #
-# @router.get("/register", tags=["register"])
-# async def get_register():
-#     content = {"data": "get_register"}
+# from fastapi import APIRouter, Request, Response
+#
+# router = APIRouter()
+#
+#
+# @router.get("/", tags=["index"])
+# async def start(request: Request) -> Response:
+#     content = json.dumps({"data": "Hello world"})
+#     return Response(
+#         content=content,
+#         status_code=200,
+#         headers=request.headers,
+#     )
+#
+# #
+# # @router.get("/register", tags=["register"])
+# # async def get_register():
+# #     content = {"data": "get_register"}
+# #     return content
+# #
+# #
+# # @router.post("/register", tags=["register"])
+# # async def post_register():
+# #     content = {
+# #         "status": 200,
+# #         "message": "success",
+# #         "data": "post_register",
+# #     }
+# #     return content
+#
+#
+# @router.get("/user/{user_nickname}")
+# async def get_user(user_nickname: str):
+#     content = [user for user in users if user.get("nickname").lower() == user_nickname.lower()]
 #     return content
-#
-#
-# @router.post("/register", tags=["register"])
-# async def post_register():
-#     content = {
-#         "status": 200,
-#         "message": "success",
-#         "data": "post_register",
-#     }
-#     return content
-
-
-@router.get("/user/{user_nickname}")
-async def get_user(user_nickname: str):
-    content = [user for user in users if user.get("nickname").lower() == user_nickname.lower()]
-    return content
